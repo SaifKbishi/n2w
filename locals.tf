b418cf901d2e89d3fcd7b38d83b25f04f1095cf0
Saif Kbishi <saifkbishi@gmail.com>
2025-07-31 15:20:56 +0300

    merge n2w permissions policies

--- locals.tf
+++ locals.tf
@@ -6,7 +6,14 @@ locals {
 
   #s3_bucket_name = "globo-web-app-${random_integer.s3.result}"
 
-  
+  policy1 =  jsondecode(file("./permissions /aws_policy_permissions_Enterprise_BYOL_1.json"))
+  policy2 =  jsondecode(file("./permissions /aws_policy_permissions_Enterprise_BYOL_2.json"))
+  policy3 =  jsondecode(file("./permissions /aws_policy_permissions_Enterprise_BYOL_3.json"))
+
+  merged_policy = {
+    Version = "2012-10-17"
+    Statement = concat(local.policy1.Statement, local.policy2.Statement, local.policy3.Statement)
+  }
 }
 
 
